dcf823d31e14a2e76cd2d480d01beeb8b3d5da0e
jisooyu <jisooyu@hotmail.com>
2021-11-23 15:56:31 +0900

    middleware: userSchema.pre useSchema.statics.findByCredentials

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -29,6 +30,32 @@ const userSchema = new mongoose.Schema({
             }
         }
     }
+})
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new Error('로그인 할 수가 없습니다.')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        throw new Error ('로그인 할 수가 없습니다.')
+    } 
+
+    return user
+}
+
+userSchema.pre('save', async function (next){
+    const user = this
+
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
