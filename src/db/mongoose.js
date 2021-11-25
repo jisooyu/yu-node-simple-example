a40018b38fde030b0c241f162d68d9664adfee3d
jisooyu <jisooyu@hotmail.com>
2021-11-25 15:09:05 +0900

    applying jwt to signup and login

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/jisooyu-task', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    // useCreateIndex: true,
-    // userFindAndModify: false
-})
+mongoose.connect('mongodb://127.0.0.1:27017/jisooyu-task')
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -29,8 +30,33 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password can not contain "password"')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.pre('save', async function (next){
+    const user = this
+
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+userSchema.methods.generateAuthToken = async function (){
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat ({ token })
+    await user.save()
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({email})
@@ -48,15 +74,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
-userSchema.pre('save', async function (next){
-    const user = this
 
-    if(user.isModified('password')){
-        user.password = await bcrypt.hash(user.password, 8)
-    }
-
-    next()
-})
 
 const User = mongoose.model('User', userSchema)
 
