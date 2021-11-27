b20097415a4fa3157c3a5fdc92167394328b2920
jisooyu <jisooyu@hotmail.com>
2021-11-27 09:22:24 +0900

    adding auth w/o exposing all user data

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,10 +26,62 @@ router.post('/users/login', async(req, res)=>{
 })
 
 
-router.get('/users', async (req, res)=> {
-    try{
-        const users = await User.find({})
-        res.send(users)
+router.get('/users/me', auth, async (req, res)=> {
+   res.send(req.user)
+})
+
+
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
     } catch (e) {
         res.status(500).send()
     }
